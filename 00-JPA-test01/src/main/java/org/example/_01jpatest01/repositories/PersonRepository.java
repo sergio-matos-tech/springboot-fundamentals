17e59f3ebfb3bd0a5bb88d2ab19fafa4d2afbec7
Sergio Henrique <sergimmatos@hotmail.com>
2025-09-25 18:17:39 -0300

    Minor ajusting in the files

--- 00-JPA-test01/src/main/java/org/example/_01jpatest01/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example._01jpatest01.repositories;
-
-import org.example._01jpatest01.domain.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-
-}
